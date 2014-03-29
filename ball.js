08857f368645b9af9f0706e25e02effd9a728ea0
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-29 10:22:14 +0100

    add bouncing X

--- ball.js
+++ ball.js
@@ -2,7 +2,7 @@ var ball = {};
 
 ball.radius = 15;
 
-ball.incX = 0;
+ball.incX = 5;
 ball.incY = 5;
 ball.dirX = 1;
 ball.dirY = 1;
@@ -22,12 +22,22 @@ ball.init = function(color) {
   this.createShape(color);
 };
 
-ball.bounce = function(CANVAS) {
+ball.bounceY = function(CANVAS) {
   if ( (this.y >= CANVAS.height - this.radius && this.dirY == 1)
     || (this.y <= this.radius && this.dirY == -1))
     this.dirY *= -1;
 };
 
+ball.bounceX = function(CANVAS){
+   if ( (this.x >= CANVAS.width - this.radius && this.dirX == 1)
+    || (this.x <= this.radius && this.dirX == -1))
+    this.dirX *= -1;
+}
+
+ball.bounce = function(CANVAS) {
+  this.bounceY(CANVAS);
+  this.bounceX(CANVAS);
+};
 
 ball.update = function(CANVAS){
   this.x += this.incX * this.dirX;
