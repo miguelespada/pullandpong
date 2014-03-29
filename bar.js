d5d946a1a2080a88f50f67c14109137491f7f807
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-29 10:58:10 +0100

    define bar class

--- bar.js
+++ bar.js
@@ -1,1 +1,15 @@
-bar = {};
+function bar(color){
+  this.color = color;
+  this.incY = 10;
+  this.width = 15;
+  this.height = 80;
+  
+  this.init = function(x, y){
+    this.x = x;
+    this.y = y;
+    this.shape = new createjs.Shape();
+    this.shape.graphics.beginFill(this.color)
+              .drawRect(this.x, this.y, this.width, this.height)
+              .endFill();
+   };
+};
