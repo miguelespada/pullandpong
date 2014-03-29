af0fdc575de18ed9461b8723fade664e3264c8eb
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-29 11:19:04 +0100

    define bar class bar

--- bar.js
+++ bar.js
@@ -1,15 +1,32 @@
-function bar(color){
-  this.color = color;
+function bar(){
   this.incY = 10;
   this.width = 15;
   this.height = 80;
-  
-  this.init = function(x, y){
-    this.x = x;
-    this.y = y;
+
+  this.init = function(color){
+    this.color = color;
     this.shape = new createjs.Shape();
     this.shape.graphics.beginFill(this.color)
-              .drawRect(this.x, this.y, this.width, this.height)
-              .endFill();
+         .drawRect(0, 0, this.width, this.height)
+         .endFill();
+  };
+
+  this.setInitialPosition = function(x, y){
+    this.x = x;
+    this.y = y;
    };
+
+   this.move = function(dir){
+    this.y += this.incY * dir;
+   };
+
+   this.update = function(CANVAS){
+      if (CANVAS.up) this.move(-1);
+      if (CANVAS.down) this.move(1);
+   };
+
+   this.draw = function(){
+    this.shape.x = this.x;
+    this.shape.y = this.y;
+  };
 };
